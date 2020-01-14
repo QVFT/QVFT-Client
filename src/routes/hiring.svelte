7d9bd87df0fc3fa24f012e51e14681ea49f8ad7a
pjsingal <59570055+pjsingal@users.noreply.github.com>
2020-01-14 13:01:31 -0500

    Create Hiring page route

---
+++ src/routes/hiring.svelte
@@ -0,0 +1,76 @@
+<script>
+  import { onMount } from "svelte";
+  import PageHeader from "../components/PageHeader.svelte";
+  onMount(async () => {
+    gtag("config", "UA-93549235-6", { page_path: "/hiring" });
+  });
+</script>
+
+<style>
+  div.pdfWrapper {
+    margin-top: 2rem;
+    width: 100%;
+    height: 95vh;
+  }
+  a.button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 3rem;
+    padding: 0.5rem 1rem;
+    font-size: 1.2rem;
+    line-height: 1.4rem;
+    color: white;
+    text-decoration: none;
+    background-color: var(--themeColorDark);
+    border-radius: 0.5rem;
+    box-shadow: 1px 1px 2px 2px #dedededd;
+  }
+  .contentWrapper {
+    margin-top: 2rem;
+  }
+  .mobile {
+    display: none;
+    justify-content: center;
+  }
+  @media (max-width: 700px) {
+    .mobile {
+      display: flex !important;
+    }
+    .desktop {
+      display: none;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Queen's Vertical Farming Team</title>
+</svelte:head>
+<PageHeader>Hiring</PageHeader>
+<div class="contentWrapper desktop">
+  <p>
+    <a
+      href="QVFT_Hiring.pdf"
+      download
+      on:click={() => gtag('event', 'download hiring package')}>
+      Download our hiring package here
+    </a>
+  </p>
+  <div class="pdfWrapper">
+    <object
+      width="100%"
+      height="100%"
+      title="Hiring"
+      data="QVFT_Hiring.pdf"
+      type="application/pdf">
+      <embed
+        title="Hiring"
+        width="100%"
+        src="QVFT_Hiring.pdf"
+        type="application/pdf" />
+    </object>
+  </div>
+</div>
+<div class="contentWrapper mobile">
+  <a class="button" href="QVFT_Hiring.pdf">View Hiring Package</a>
+</div>
