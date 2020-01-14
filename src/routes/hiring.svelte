f1138e28acc6800a2b98958cae15d21c6415f634
pjsingal <59570055+pjsingal@users.noreply.github.com>
2020-01-14 15:05:10 -0500

    Added application form

--- src/routes/hiring.svelte
+++ src/routes/hiring.svelte
@@ -20,7 +20,7 @@
     padding: 0.5rem 1rem;
     font-size: 1.2rem;
     line-height: 1.4rem;
-    color: white;
+    color: red;
     text-decoration: none;
     background-color: var(--themeColorDark);
     border-radius: 0.5rem;
@@ -46,14 +46,19 @@
 <svelte:head>
   <title>Queen's Vertical Farming Team</title>
 </svelte:head>
+
 <PageHeader>Hiring</PageHeader>
-<div class="contentWrapper desktop">
+ <div class="contentWrapper desktop">
+  <h2>Description</h2>
+  <p>
+    We are currently seeking four passionate, conscientious, creative thinkers to join our team for Winter 2020. We are hiring two mechanical designers, and two software engineers. Please see the Hiring Package for further details. Applications close Friday, January 17th at 11:59 PM.
+  </p>
   <p>
     <a
       href="QVFT_Hiring.pdf"
       download
       on:click={() => gtag('event', 'download hiring package')}>
-      Download our hiring package here
+      Click to Download Hiring Package
     </a>
   </p>
   <div class="pdfWrapper">
@@ -69,6 +74,9 @@
         src="QVFT_Hiring.pdf"
         type="application/pdf" />
     </object>
+  </div>
+  <div>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd5uvv4FCd4I6nbvfFewKybUq8MBjRVlDWuwkxcO0t1d8tw8w/viewform?embedded=true" width="640" height="2891" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
   </div>
 </div>
 <div class="contentWrapper mobile">
