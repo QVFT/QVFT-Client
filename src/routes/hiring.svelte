4053258a4237460aaaa14082ea17ec2bca786161
pjsingal <59570055+pjsingal@users.noreply.github.com>
2020-01-14 16:39:04 -0500

    Adjusted pdf/form heights

--- src/routes/hiring.svelte
+++ src/routes/hiring.svelte
@@ -21,7 +21,7 @@
   div.pdfWrapper {
     margin-top: 2rem;
     width: 100%;
-    height: 60vh;
+    height: 70vh;
   }
 
   .contentWrapper {
@@ -97,7 +97,7 @@
   <iframe
     src="https://docs.google.com/forms/d/e/1FAIpQLSd5uvv4FCd4I6nbvfFewKybUq8MBjRVlDWuwkxcO0t1d8tw8w/viewform?embedded=true"
     width="100%"
-    height="475"
+    height="485"
     frameborder="0"
     marginheight="0"
     marginwidth="0"
