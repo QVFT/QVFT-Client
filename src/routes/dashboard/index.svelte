80d2c11b8e9ab574e4282e8e1ba77df4a83b0640
Ross Hill <rosshill@protonmail.com>
2020-01-21 14:48:53 -0500

    Add dashboard page

---
+++ src/routes/dashboard/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import PageHeader from "../../components/PageHeader.svelte";
+</script>
+
+<PageHeader>Dashboard</PageHeader>
+<div class="contentWrapper">
+  <h3>Overview</h3>
+  The is the QVFT Dashboard, where you can view environment variables and set
+  environment targets
+</div>
