b0e9be5a0210c343ad35ebc80c3a0f395f885496
pjsingal <59570055+pjsingal@users.noreply.github.com>
2020-02-04 16:14:26 -0500

    Removed info package from ceec

--- src/routes/ceec-survey.svelte
+++ src/routes/ceec-survey.svelte
@@ -42,12 +42,6 @@
 
 <PageHeader>CEEC Survey</PageHeader>
 <div class="contentWrapper">
-  <h2></h2>
-  <div id="infoPackageSection">
-    <div class="buttonWrapper">
-      <a class="button" href="QVFT_Information.pdf">View Information Package</a>
-    </div>
-  </div>
   <iframe
     src="https://docs.google.com/forms/d/e/1FAIpQLScNUqCycnsKIWjNFad4DiHPWmsiJ-eqbMDS3HqOxbTdMmAfeQ/viewform?embedded=true"
     width="100%"
