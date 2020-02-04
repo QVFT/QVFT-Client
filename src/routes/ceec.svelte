214f3eab3adb622cbbde902fa52ebb9cd3eaab9e
pjsingal <59570055+pjsingal@users.noreply.github.com>
2020-02-04 04:12:00 -0500

    Update ceec.svelte

--- src/routes/ceec.svelte
+++ src/routes/ceec.svelte
@@ -9,8 +9,8 @@
 <style>
   div.pdfWrapper {
     margin-top: 2rem;
-    width: 60%;
-    height: 60vh;
+    width: 100%;
+    height: 70vh;
   }
   .contentWrapper {
     margin-top: 2rem;
@@ -92,7 +92,7 @@
   <iframe 
     src="https://docs.google.com/forms/d/e/1FAIpQLScNUqCycnsKIWjNFad4DiHPWmsiJ-eqbMDS3HqOxbTdMmAfeQ/viewform?embedded=true" 
     width="100%" 
-    height="2000" 
+    height="1600" 
     frameborder="0" 
     marginheight="0" 
     marginwidth="0"
