cc4229573521a071f0a7ddc8a8b7ca139c06360a
Ross Hill <rosshill@protonmail.com>
2020-02-04 16:04:02 -0500

    Change ceec page

--- src/routes/ceec-survey.svelte
+++ src/routes/ceec-survey.svelte
@@ -7,21 +7,12 @@
 </script>
 
 <style>
-  div.pdfWrapper {
-    margin-top: 2rem;
-    width: 100%;
-    height: 70vh;
-  }
   .contentWrapper {
     margin-top: 2rem;
   }
-  
+
   iframe {
     margin-bottom: 2rem;
-  }
-  .mobile {
-    display: none;
-    justify-content: center;
   }
   div.buttonWrapper {
     text-align: center;
@@ -40,19 +31,8 @@
     border-radius: 0.5rem;
     box-shadow: 1px 1px 2px 2px #dedededd;
   }
-  .mobile {
-    display: none;
-  }
   div#infoPackageSection {
     margin-bottom: 5rem;
-  }
-  @media (max-width: 700px) {
-    .mobile {
-      display: block !important;
-    }
-    .desktop {
-      display: none;
-    }
   }
 </style>
 
@@ -64,30 +44,16 @@
 <div class="contentWrapper">
   <div id="infoPackageSection">
     <h2>Information Package</h2>
-    <div class="pdfWrapper desktop">
-      <object
-        width="100%"
-        height="100%"
-        title="Information"
-        data="QVFT_Information.pdf"
-        type="application/pdf">
-        <embed
-          title="Information"
-          width="100%"
-          src="QVFT_Information.pdf"
-          type="application/pdf" />
-      </object>
-    </div>
-    <div class="buttonWrapper mobile">
+    <div class="buttonWrapper">
       <a class="button" href="QVFT_Information.pdf">View Information Package</a>
     </div>
   </div>
-  <iframe 
-    src="https://docs.google.com/forms/d/e/1FAIpQLScNUqCycnsKIWjNFad4DiHPWmsiJ-eqbMDS3HqOxbTdMmAfeQ/viewform?embedded=true" 
-    width="100%" 
-    height="1875" 
-    frameborder="0" 
-    marginheight="0" 
+  <iframe
+    src="https://docs.google.com/forms/d/e/1FAIpQLScNUqCycnsKIWjNFad4DiHPWmsiJ-eqbMDS3HqOxbTdMmAfeQ/viewform?embedded=true"
+    width="100%"
+    height="1875"
+    frameborder="0"
+    marginheight="0"
     marginwidth="0"
     title="Questionnaire">
     Loading…
