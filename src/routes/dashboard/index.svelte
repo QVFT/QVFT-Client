1c97ceed0f8a9519ca981dc61aa5ff6690660593
Ross Hill <rosshill@protonmail.com>
2020-02-10 19:39:20 -0500

    Add chart js
    Co-authored-by: M Wrana <wranamichael@gmail.com>

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,12 +1,23 @@
 <script>
+  import { onMount } from "svelte";
   import PageHeader from "../../components/PageHeader.svelte";
+
+  import Chart from "chart.js";
+
+  onMount(() => {
+    var ctx = document.getElementById("myChart").getContext("2d");
+
+    data: var myLineChart = new Chart(ctx, {
+      type: "line",
+      data: [{ x: 10, y: 20 }, { x: 15, y: 10 }]
+    });
+  });
 </script>
 
 <PageHeader>Dashboard</PageHeader>
 <div class="contentWrapper">
   <h3>Overview</h3>
-  <p>
-    The is the QVFT Dashboard, where you can view environment variables and set
-    environment targets
-  </p>
+  <div>
+    <canvas id="myChart" width="400" height="400" />
+  </div>
 </div>
