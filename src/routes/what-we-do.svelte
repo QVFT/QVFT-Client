2092a766ab265d3f9f42407de5ca09d97d3a3cc5
pjsingal <59570055+pjsingal@users.noreply.github.com>
2021-09-02 15:51:42 -0400

    Updated text, updated images, reordered contents

--- src/routes/what-we-do.svelte
+++ src/routes/what-we-do.svelte
@@ -76,6 +76,61 @@
 </svelte:head>
 
 <PageHeader>What We Do</PageHeader>
+
+  <h2>Background</h2>
+  <p>
+    A global trend of increasing concern is the diminishing supply of arable land per capita. Due to trends such as climate change, freshwater depletion, and soil degradation, arable land per capita will fall to one-third of the amount available in 1970 by 2050. The unsustainable practices of conventional agriculture exacerbate these issues. In addition, the world population is expected to increase from 7.7 billion (2019) to 9.7 billion (2050). The intersection of these climate and population challenges means that global food security depends on our ability to adapt to increased demand and develop better farming techniques. 
+  <p>
+    Vertical farming is a cultivation practice in which crops are grown in an indoor, climate- controlled facility. This approach is associated with dramatically reduced water consumption (~95%), minimal transportation costs, and massive improvements in per-acre land productivity. Vertical farming can grow nutritious, organic produce in any location, at any time of year. Given these advantages, this technology is projected to become a major contributor to global food production in the coming decades. The vertical farming industry’s global market value is projected to grow from $2.23 billion in 2018 to $12.77 billion by 2026, representing a compounded annual growth rate of 24.6%.
+  </p>
+
+  <h2>Aeroponic System</h2>
+  <p>
+    QVFT employs an aeroponic cultivation method, in which plants grow without soil and are fed by a nutrient-enriched mist. Crops rest in thin, plastic root cups, through which their roots hang into a basin below. Aeroponic vertical farming lends well to automation and can allow for near- complete control over the plant growth environment. The system is broadly divided into five zones: mixing tank, nutrient zone, supply line, growth zone, and return line.
+  </p>
+   <div class="imageWrapper large padding">
+    <picture class="CAD Model">
+      <source srcset="CAD Model.webp" type="image/webp" />
+      <!-- <source srcset="CAD Model.png" type="image/png" /> -->
+      <!-- <img src="CAD Model.png" alt="CAD Model" />  -->
+      <img src="CAD Model.webp" alt="CAD Model" />
+    </picture>
+    <div class="dark_attribution" rel="noopener noreferrer" target="_blank">
+      QVFT
+    </div>
+     
+  </div>
+  <h2>Automation Subsystem & Software Stack</h2>
+  <p>
+    The automation subsystem is an electromechanical feedback control system that automatically maintains optimal growth conditions using interconnected sensors (inputs) and actuators (outputs). These components interact through a logical framework programmed into their Arduino microcontrollers. At the same time, sensor readings will be continuously logged by a computer located on-site. The computer will reformat the data to be human-readable and then upload it to a website via Ethernet.
+  </p>
+  The software stack entails the database, back-end, and front-end and ultimately aims to provide a publicly accessible repository of real-time vertical farming data. This repository achieves three objectives: (i) Allows technicians to remotely monitor farm equipment, which improves operations and resource allocation efficiency. (ii) Provides a record for future tuning and improvement of the farm by comparing data to commercial vertical farms and conventional producers. (iii) Creates a public knowledge base to assist other groups with vertical farming research. The majority of vertical farming research is currently conducted by private companies and is thus inaccessible to the public. QVFT’s mission is to democratize and advance scientific knowledge in this rapidly developing field.
+  </p>
+
+  <div class="imageWrapper padding">
+    <picture class="diagram">
+      <source srcset="CEA-diagram.webp" type="image/webp" />
+      <source srcset="CEA-diagram.png" type="image/png" />
+      <img src="CEA-diagram.webp" alt="CEA system digram" />
+    </picture>
+    <div class="dark_attribution" rel="noopener noreferrer" target="_blank">
+      QVFT
+    </div>
+  </div>
+
+  </p>
+
+  <div class="imageWrapper padding">
+    <picture class="Automation_and_Circuits">
+      <source srcset="Automation_and_Circuits.webp" type="image/webp" />
+      <source srcset="Automation_and_Circuits.jpeg" type="image/jpeg" />
+      <img src="Automation_and_Circuits.webp" alt="Automation_and_Circuits" />
+    </picture>
+    <div class="dark_attribution" rel="noopener noreferrer" target="_blank">
+      QVFT
+    </div>
+  </div>
+
 <div class="contentWrapper">
   <h2>System Guide</h2>
 
@@ -84,7 +139,7 @@
       href="QVFT_System_Guide.pdf"
       download
       on:click={() => gtag('event', 'download system guide')}>
-      Download our System Guide here
+      Download our complete System Guide here
     </a>
   </p>
   <div class="pdfWrapper">
@@ -101,101 +156,21 @@
         type="application/pdf" />
     </object>
   </div>
-  <h2>Background</h2>
-  <p>
-    A global trend of increasing concern is the diminishing supply of arable
-    land per capita. The United Nations Food and Agriculture Organization (FAO)
-    projects that by 2050, this supply will fall to one third of the amount
-    available in 1970. This is a combined result of climate change, freshwater
-    depletion, urbanization, soil depletion, soil degradation, and a world
-    population expected to increase from 7.7 billion (2019) to 9.7 billion
-    (2050).
-  </p>
-  <p>
-    An emerging technology which is addressing these concerns is vertical
-    farming, a food cultivation method in which crops grow in an indoor, urban,
-    climate-controlled facility. This approach is associated with dramatically
-    reduced water consumption, slashed transportation costs, massive
-    improvements in per-acre land productivity, increased plant productivity,
-    and the freedom to cultivate crops in any location, year-round.
-  </p>
-
-  <h2>Aeroponics</h2>
-  <p>
-    QVFT employs an aeroponic cultivation method, in which nutrients are
-    dissolved in water (fertigation) and misted diectly onto roots via spray
-    nozzles (irrigation). Crops rest on a thin, porous substrate, through which
-    their roots dangle in a basin below. Requiring no soil, this approach allows
-    the grower near-complete control over the specific nutrient mix a plant
-    receives. Aeroponics are a key component of controlled-environment
-    agriculture.
-  </p>
-   <div class="imageWrapper large padding">
-    <picture class="CAD Model">
-      <source srcset="CAD Model.webp" type="image/webp" />
-      <!-- <source srcset="CAD Model.png" type="image/png" /> -->
-      <!-- <img src="CAD Model.png" alt="CAD Model" />  -->
-      <img src="CAD Model.webp" alt="CAD Model" />
-    </picture>
-    <div class="dark_attribution" rel="noopener noreferrer" target="_blank">
-      QVFT
-    </div>
-     
-  </div>
-  <h2>Controlled-Environment Agriculture (CEA)</h2>
-  <p>
-    CEA is a method of optimizing plant growth conditions by means of an
-    integrated, software-controlled sensory network. Our proposed system (see
-    below) aims to control, monitor, and maximize farming yield by comparing
-    realtime environmental data to pre-set environmental targets, and adjusting
-    the internal growing environment accordingly. The CEA system will serve as
-    the central control hub for the lighting, irrigation, fertigation,
-    ventilation, and climate control systems.
-  </p>
-  <div class="imageWrapper padding">
-    <picture class="diagram">
-      <source srcset="CEA-diagram.webp" type="image/webp" />
-      <source srcset="CEA-diagram.png" type="image/png" />
-      <img src="CEA-diagram.png" alt="CEA system digram" />
-    </picture>
-    <div class="dark_attribution" rel="noopener noreferrer" target="_blank">
-      QVFT
-    </div>
-  </div>
-  <h2>Lighting</h2>
-  <p>
-    Studies have found that the growth of indoor plants can be manipulated by
-    adjustments to the intensity and wavelength of LED lights. Our plant science
-    team is conducting research which aims to optimize these parameters for each
-    given crop. Tying into the development of the broader CEA system, the
-    ultimate goal of these efforts is to boost plant productivity.
-  </p>
-  <h2>Crop Selection</h2>
-  <p>
-    As vertical farming is a cost-intensive cultivation method, current industry
-    practices focus on small, premium crops that have short growth cycles and
-    high automation potential. Examples include leafy greens such as baby
-    lettuce, spinach, and kale, which have a brief growth cycle of roughly 50–65
-    days. QVFT will focus exclusively on the cultivation of leafy greens, as the
-    commercial farming systems from which we draw inspiration are designed
-    specifically for such purposes. Starting in February 2020, we will cultivate
-    a small batch of baby lettuce as a litmus test for our prototype. This will
-    provide feedback regarding the efficacy of our system and identify areas of
-    improvement for when we scale our model in the following school year.
-  </p>
 </div>
 
-  <div class="imageWrapper">
-    <picture class="diagram">
-      <source srcset="ZipGrow.webp" type="image/webp" />
-      <source srcset="ZipGrow.jpg" type="image/jpeg" />
-      <img src="ZipGrow.webp" alt="Queen's Vertical Farming Team" />
-    </picture>
-    <a
-      class="attribution"
-      rel="noopener noreferrer"
-      href="https://zipgrow.com/"
-      target="_blank">
-      ZipGrow.com
-    </a>
-  </div>
+<div class="imageWrapper">
+  <picture class="diagram">
+    <source srcset="ZipGrow.webp" type="image/webp" />
+    <source srcset="ZipGrow.jpg" type="image/jpeg" />
+    <img src="ZipGrow.webp" alt="Queen's Vertical Farming Team" />
+  </picture>
+  <a
+    class="attribution"
+    rel="noopener noreferrer"
+    href="https://zipgrow.com/"
+    target="_blank">
+    ZipGrow.com
+  </a>
+</div>
+
+
